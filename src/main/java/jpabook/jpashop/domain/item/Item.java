efdc942d6451461f33abcb2973ebcf5ba38bec96
cwkim723 <jerry.pref@gmail.com>
2022-03-09 22:27:58 +0900

    feat: 엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dType")
+@Getter @Setter
+public abstract class Item {
+
+    /*
+        [ InheritanceType ]
+        SINGLE_TABLE: 한 테이블에 다 때려박음 => 이거 선택할 것
+        TABLE_PER_CLASS: BOOK, MOVIE, ALBUM 이렇게 나뉘어져 있음
+        JOINED
+    */
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+}
