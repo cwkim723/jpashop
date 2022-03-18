b8f62f26ccd21ce66f322de7a9ee50629bfd8d04
cwkim723 <jerry.pref@gmail.com>
2022-03-18 14:54:25 +0900

    feat: 회원 등록 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -19,8 +19,31 @@ public class MemberApiController {
     @PostMapping("api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         // @RequestBody: json으로 온 바디를 그대로 멤버로 바꿔줌
+        // @Valid: Member를 검증해준다.
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    /**
+     * v2: 요청값으로 별도의 dto를 받는 api
+     * @param request
+     * @return
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+
+        // 누가 엔티티를 바꿔도 api 스펙이 바뀌지 않음!
+    }
+
+    // dto
+    @Data
+    static class CreateMemberRequest{
+        private String name;
     }
 
     @Data
